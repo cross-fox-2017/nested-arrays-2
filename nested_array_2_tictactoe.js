9652793c8b5ed48dff5eeb7bb56739fe8cb4dcb7
qblol <iqbal.0809@gmail.com>
2017-01-04 20:12:22 +0700

    tictactoe done

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,15 +1,32 @@
 "use strict"
 
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  console.log("kitaaa lagi jalan")
+  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X", "O"]
   var table = []
+  var countO = 0;
+  var countX = 0;
   for (let i = 0; i < 3; i++) {
     table.push([]);
-    table[i].push(xo_random[Math.floor(Math.random() * 9)]);
-    table[i].push(xo_random[Math.floor(Math.random() * 9)]);
-    table[i].push(xo_random[Math.floor(Math.random() * 9)]);
+    table[i].push(xo_random[Math.floor(Math.random() * 10)]);
+    table[i].push(xo_random[Math.floor(Math.random() * 10)]);
+    table[i].push(xo_random[Math.floor(Math.random() * 10)]);
   }
-  return table;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (table[i][j] == "X") {
+        countX += 1;
+      } else {
+        countO += 1;
+      }
+    }
+  }
+
+  if (countX > 5 || countO > 5) {
+    return tic_tac_toe_board();
+  } else {
+    return table;
+  }
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
