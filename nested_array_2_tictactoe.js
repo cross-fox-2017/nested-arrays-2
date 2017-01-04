b649ffda6d54d2d28a0ab8d42875800804655503
Syanmil <syanmil@yahoo.com>
2017-01-04 14:01:44 +0700

    populating tic_tac_toe_board done, still static

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,13 @@
 function tic_tac_toe_board() {
   var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var board = [];
+  for (var i = 0; i < 3; i++) {
+    board[i] = []
+    for (var j = 0; j < 3; j++){
+      board[i].push(xo_random[j*i])
+    }
+  }
+  return board
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
