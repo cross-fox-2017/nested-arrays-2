5c2520e2620f62b15f6b7a24fa060d42c4e19217
qblol <iqbal.0809@gmail.com>
2017-01-04 17:30:50 +0700

    tictactoe

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,15 @@
+"use strict"
+
 function tic_tac_toe_board() {
   var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var table = []
+  for (let i = 0; i < 3; i++) {
+    table.push([]);
+    table[i].push(xo_random[Math.floor(Math.random() * 9)]);
+    table[i].push(xo_random[Math.floor(Math.random() * 9)]);
+    table[i].push(xo_random[Math.floor(Math.random() * 9)]);
+  }
+  return table;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
