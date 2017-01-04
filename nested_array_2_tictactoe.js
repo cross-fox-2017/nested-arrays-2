5fe33889f723eee35f3627fd497a75afd49baf1b
Eri Irawan & Raditya Arya <pair+mrei91.radityaarya@hacktiv8.com>
2017-01-04 21:55:04 +0700

    raditya_eri

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,35 @@
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
+  let x = 0;
+  let o = 0;
+  let board = [[],[],[]];
+
+  for (let i = 0; i < 3; i++) {
+    let board =[]
+    for (let j = 0; j < 3; j++) {
+      board.push([])
+      let random = Math.random();
+      if( x < 5 && o < 5){
+        if (random > 0.5) {
+          x++;
+          board[i].push('X')
+        }
+        else {
+          o++;
+          board[i].push('O')
+        }
+      }
+      else {
+        if (x == 5) {
+          board[i].push('O')
+        }
+        else {
+          board[i].push('X')
+        }
+      }
+    }
+  }
+  return board;
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+console.log(tic_tac_toe_board()) // => make a random tic tac toe board
