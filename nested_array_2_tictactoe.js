4fa91fff02d87f716fcd398d8ed2b8f389396049
Syanmil <syanmil@yahoo.com>
2017-01-04 14:48:12 +0700

    adding randomer?

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,6 +1,6 @@
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
   var board = [];
+  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
   for (var i = 0; i < 3; i++) {
     board[i] = []
     for (var j = 0; j < 3; j++){
@@ -9,5 +9,9 @@ function tic_tac_toe_board() {
   }
   return board
 }
+function randomer (){
+  return Math.floor(Math.random()*xo_random.length)
+}
 
+console.log(randomer());
 console.log(tic_tac_toe_board()) // => make a random tic tic board
