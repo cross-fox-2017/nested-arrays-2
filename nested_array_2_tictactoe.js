e943dd33e8000823f9a0580160d02343b24981a2
unknown <yaumashf@gmail.com>
2017-01-04 16:29:16 +0700

    extra credit release 0

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -11,3 +11,16 @@ function tic_tac_toe_board() {
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
+
+function test() {
+	let data = tic_tac_toe_board();
+		data = data.join('').split(',').join('');
+		x = data.split('O').join('') .length;
+		o = data.split('X').join('') .length;
+	if((o===4||o===5)&&(x===4||x===5)){
+		return true;
+	}	
+	return false;
+} 
+
+console.log(test());
