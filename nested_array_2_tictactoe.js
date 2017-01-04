3d95780d88e09633d39e3aac67028064d0601354
unknown <yaumashf@gmail.com>
2017-01-04 16:10:18 +0700

    release 0

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,13 @@
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+	var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+	var tictac = [];
+	var i, j, chunk = 3;
+
+	for(i=0, j=xo_random.length; i<j; i+=chunk){
+		tictac.push(xo_random.slice(i, i+chunk));
+	}
+
+	return tictac;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
