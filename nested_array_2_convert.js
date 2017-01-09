077e727dec60bd8f71bca32aedfd7e476bbd6bbf
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-09 07:37:56 +0700

    done

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -10,7 +10,26 @@ var roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format(nestedArray) {
-    // your convert code here
+    var obj = [];
+    var value = [[],[],[],[]];
+    var column = roster[0];
+    for(var i = 0; i < roster.length; i++){
+    	// for (var k = 0; k < roster.length; k++){
+    	// 	value[k].push(roster[i][k]);
+    	// }
+    	value[0].push(roster[i][0]);
+    	value[1].push(roster[i][1]);
+    	value[2].push(roster[i][2]);
+    	value[3].push(roster[i][3]);
+    }
+    for(var j = 0; j < value.length; j++){
+    	var temp2 ={};
+    	for(var k=0; k < column.length; k++){
+    		temp2[column[k]] = value[k][j];
+    	}
+    	obj.push(temp2);
+    }
+    return obj;
 }
 
 var object_roster = convert_roster_format(roster)
@@ -18,4 +37,4 @@ console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]["Name"] == "Joe Schmo") // outputs true
+console.log(object_roster[1]["Name"] == "Joe Schmo") // outputs true
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,25 @@
 function tic_tac_toe_board() {
   var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var tampung = [];
+  var countX = 0;
+  var countO = 0;
+  for (var i = 0; i < 9; i++){
+  	var random = Math.floor((Math.random() * 2) + 1);
+  	if(random == 1){
+  		if(countX !== 5){
+  			tampung.push("X");
+  			countX++
+  		}
+  	}
+  	else if (random == 2){
+  		if(countO !== 5){
+  			tampung.push("O");
+  			countO++;
+  		}
+  	}
+  }
+  	return tampung;
 }
-
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+ 
+// console.log(tic_tac_toe_board()) // => make a random tic tic board
+console.log(tic_tac_toe_board());
